e870c15c087d7d8bf3bb9cc2dff02b3717f0e04b
Adam Lamson <lamsoa729@gmail.com>
2021-04-29 15:53:28 -0400

    I wrote a test ^_^

--- tests/test_exercise.py
+++ tests/test_exercise.py
@@ -1,20 +1,27 @@
 import pytest
 from sciware_testing_python import sum_numbers, add_vectors
 
+
 def test_sum_numbers_123():
     # basic test to see if we get the expected answer for a simple case.
-    sum = sum_numbers([1,2,3])
+    sum = sum_numbers([1, 2, 3])
     assert sum == 6
+
 
 def test_sum_numbers_yours():
     # write another test of the sum_numbers function
+    sum = sum_numbers([1, -2, 9876543])
+    assert sum == 9876542
     pass
+
 
 def test_sum_numbers_empty():
     # what's the sum of an empty list?
     pass
 
-#@pytest.mark.xfail(strict=True, raises=TypeError)
+# @pytest.mark.xfail(strict=True, raises=TypeError)
+
+
 def test_sum_strings():
     #assert sciware_testing_python.sum_numbers(["1","2","3"]) == "123"
     pass
